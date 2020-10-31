955b33a403b0290d5d8db1e016c337a5d9f2be19
grishalisov <55431729+grishalisov@users.noreply.github.com>
2020-10-31 19:54:13 +0500

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Talent.Views;
+using Xamarin.Forms;
+
+namespace Talent
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            this.CurrentItem.CurrentItem = homePage;
+            Routing.RegisterRoute(nameof(TargetPage), typeof(TargetPage));
+            Routing.RegisterRoute(nameof(MessPage), typeof(MessPage));
+            Routing.RegisterRoute(nameof(NewsPage), typeof(NewsPage));
+            Routing.RegisterRoute(nameof(TestPage), typeof(TestPage));
+            Routing.RegisterRoute(nameof(ProfilePage), typeof(ProfilePage));
+        }
+    }
+}
