aec97564923f25b9dd6f3ff59de73d895ca1517b
grishalisov <55431729+grishalisov@users.noreply.github.com>
2020-11-01 09:03:55 +0500

    Update client.py

--- messanger/client.py
+++ messanger/client.py
@@ -1,67 +1,36 @@
-import socket, threading, time
+import socket 
+import time
 
-key = 8194
+conn = socket.socket()
+conn.connect( ("127.0.0.1", 14900) )
+conn.send(b"Hello! \n")
+data = b""
+tmp = conn.recv(1024)
+while tmp:
+    data += tmp
+    tmp = conn.recv(1024)
+print( data.decode("utf-8") )
+conn.close()
+conn = socket.socket()
+conn.connect( ("vk.com", 80) )
+conn.setblocking(0)
 
-shutdown = False
-join = False
+try: data = conn.recv(1024)
+except socket.error: 
+    pass 
+else: 
+    print(data)
+    
+conn = socket.socket()
+conn.bind( ("", 8989) )
+conn.listen(5)
+conn.setblocking(0)
 
-def receving (name, sock):
-	while not shutdown:
-		try:
-			while True:
-				data, addr = sock.recvfrom(1024)
-				
-				decrypt = ""; k = False
-				for i in data.decode("utf-8"):
-					if i == ":":
-						k = True
-						decrypt += i
-					elif k == False or i == " ":
-						decrypt += i
-					else:
-						decrypt += chr(ord(i)^key)
-				print(decrypt)
-				
-
-				time.sleep(0.2)
-		except:
-			pass
-host = socket.gethostbyname(socket.gethostname())
-port = 0
-
-server = ("",9090)
-
-s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-s.bind((host,port))
-s.setblocking(0)
-
-alias = input("Имя: ")
-
-rT = threading.Thread(target = receving, args = ("RecvThread",s))
-rT.start()
-
-while shutdown == False:
-	if join == False:
-		s.sendto(("["+alias + "] => Залетел в чатик ").encode("utf-8"),server)
-		join = True
-	else:
-		try:
-			message = input()
-
-			# Begin
-			crypt = ""
-			for i in message:
-				crypt += chr(ord(i)^key)
-			message = crypt
-			# End
-
-			if message != "":
-				s.sendto(("["+alias + "] :: "+message).encode("utf-8"),server)
-			
-			time.sleep(0.2)
-		except:
-			s.sendto(("["+alias + "] <= Вышел из чатика( ").encode("utf-8"),server)
-			shutdown = True
-
-rT.join()
-s.close()
+try: client, addr = conn.accept()
+except socket.error: 
+    pass 
+else: 
+    client.setblocking(0) 
+    parse(client, addr)
+    
+    
